315ed41c007e701148f1f0b50462466e8baa142e
levklg <myevent@mail.ru>
2022-07-03 18:04:58 +0300

    first commit

---
+++ hw01-gradle/src/main/java/HelloOtus.java
@@ -0,0 +1,21 @@
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import java.util.List;
+
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+
+        List<Integer> values = Lists.newArrayList(3, null, 4, 7,
+                8, null, 7);
+
+        Iterable<Integer> filtered = Iterables.filter(values,
+                Predicates.notNull());
+
+        for (Integer i: filtered) {
+            System.out.println(i);
+        }
+    }
+}
